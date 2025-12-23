4b3bc23c6aa1cde53427bf91ea40f8979aaf2f4a
Pawan Kumar <100339142+PawanKumar8544@users.noreply.github.com>
2025-12-23 22:27:59 +0530

    Time: 137 ms (6%), Space: 190.9 MB (8%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
@@ -1,16 +1,6 @@
-class Event {
-  public int time;
-  public int value;
-  public int isStart;
-  public Event(int time, int value, int isStart) {
-    this.time = time;
-    this.value = value;
-    this.isStart = isStart;
-  }
-}
-
 class Solution {
   public int maxTwoEvents(int[][] events) {
+    record Event(int time, int value, int isStart) {}
     int ans = 0;
     int maxValue = 0;
     Event[] evts = new Event[events.length * 2];
@@ -23,10 +13,7 @@ class Solution {
       evts[i * 2 + 1] = new Event(end + 1, value, 0);
     }
 
-    Arrays.sort(evts,
-                (a, b)
-                    -> a.time == b.time ? Integer.compare(a.isStart, b.isStart)
-                                        : Integer.compare(a.time, b.time));
+    Arrays.sort(evts, Comparator.comparingInt(Event::time).thenComparingInt(Event::isStart));
 
     for (Event evt : evts)
       if (evt.isStart == 1)
