2cfba0e36a2cb9cd95da1d29571d22cd019ea55c
Pawan Kumar <100339142+PawanKumar8544@users.noreply.github.com>
2026-08-01 22:19:57 +0530

    Time: 10 ms (63.47%), Space: 47.9 MB (72.09%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,7 +1,6 @@
 class Solution {
-  // Same as 3014. Minimum Number of Pushes to Type Word I
-  public int minimumPushes(String word) {
-    int ans = 0;
+    public int minimumPushes(String word) {
+           int ans = 0;
     int[] count = new int[26];
 
     for (final char c : word.toCharArray())
@@ -13,5 +12,5 @@ class Solution {
       ans += count[26 - i - 1] * (i / 8 + 1);
 
     return ans;
-  }
+    }
 }
